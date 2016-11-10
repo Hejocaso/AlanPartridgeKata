c6cb06198b5957880d57976626064504c7354a95
Helen Soniega <helen.soniega@digital.hmrc.gov.uk>
2016-11-10 21:24:22 +0000

    Add different number of exclamations depending on amount of matches

--- src/test/scala/AlanPartridgeSpec.scala
+++ src/test/scala/AlanPartridgeSpec.scala
@@ -12,18 +12,30 @@ class AlanPartridgeKataSpec extends FlatSpec with Matchers {
   "Calling AlanPartridgeKata" should
     "return 'Lynn, I've pierced my foot on a spike'" in {
     val unrelatedTermslist = List("Apple", "Banana", "Coconut", "Durian", "Eggplant")
-    for (i <- unrelatedTermslist) {
-      println(speakAlan.speak(i))
-      speakAlan.speak(i) shouldBe noRelatedTerm
-    }
+    val result = speakAlan.speak(unrelatedTermslist)
+    println(result)
+    result shouldBe noRelatedTerm
   }
 
   "Calling AlanPartridgeKata" should
-    "return 'Mine's a Pint!'" in {
-    val termsList = List("Partridge", "PearTree", "Chat", "Dan", "Toblerone", "Lynn", "AlphaPapa", "Nomad")
-    for (i <- termsList) {
-      println(speakAlan.speak(i))
-      speakAlan.speak(i) shouldBe relatedTermFound
-    }
+    "return 'Mine's a Pint!!'" in {
+    val termsList = List("Partridge", "PearTree")
+    val result = speakAlan.speak(termsList)
+    println(result)
+    result shouldBe "Mine's a Pint!!" //two !'s
+
+    //val termsList = List("Partridge", "PearTree", "Chat", "Dan", "Toblerone", "Lynn", "AlphaPapa", "Nomad")
+//    for (i <- termsList) {
+//      println(speakAlan.speak(i))
+//      speakAlan.speak(i) shouldBe relatedTermFound
+//    }
   }
+
+  "Calling AlanPartridge" should
+    "return 'Mine's a pint!!!!!!!!'" in {
+    val termsList = List("Partridge", "PearTree", "Chat", "Dan", "Toblerone", "Lynn", "AlphaPapa", "Nomad")
+    val result = speakAlan.speak(termsList)
+    println(result)
+    result shouldBe "Mine's a Pint!!!!!!!!"
+    }
 }
